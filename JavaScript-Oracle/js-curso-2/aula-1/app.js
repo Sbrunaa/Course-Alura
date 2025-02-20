e85989d2c9918f185439495f5e0545da7957900a
Bruna Santos <ssbru24@gmail.com>
2025-02-20 19:50:23 -0300

    atualizando

---
+++ JavaScript-Oracle/js-curso-2/aula-1/app.js
@@ -0,0 +1,9 @@
+let title = document.querySelector('h1');
+title.innerHTML = 'Jogo do número secreto';
+
+let paragraph = document.querySelector('p');
+paragraph.innerHTML = 'Escolha um número entre 1 e 10';
+
+function verificarChute(){
+    
+}
