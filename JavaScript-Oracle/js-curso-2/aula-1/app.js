e44c44d01e25c984e66d36dc2a15409894cbde16
Bruna Santos <ssbru24@gmail.com>
2025-03-18 21:09:53 -0300

    desafio

--- JavaScript-Oracle/js-curso-2/aula-1/app.js
+++ JavaScript-Oracle/js-curso-2/aula-1/app.js
@@ -1,9 +1,28 @@
 let title = document.querySelector('h1');
-title.innerHTML = 'Jogo do número secreto';
+title.innerHTML = 'Hora do desafio';
 
 let paragraph = document.querySelector('p');
 paragraph.innerHTML = 'Escolha um número entre 1 e 10';
 
 function verificarChute(){
-    
+    console.log('O botão foi clicado');
+}
+
+function alertaBotao() {
+    alert('Eu amo JS');
+}
+
+function promptBotao(){
+   let nomeCidade = prompt('Qual a cidade do Brasil?');
+
+    alert(`Estive em ${nomeCidade} e lembrei de você `);
+}
+
+function somaBotao(){
+    let numero1 = parseInt(prompt('Digite um número inteiro: '));
+    let numero2 = parseInt(prompt('Digite um segundo número inteiro: '));
+
+    let soma = numero1 + numero2;
+
+    alert(`A soma dos sois sumeros é ${soma}`);
 }
